d16850e2ec5efd64e061719932611190bc6e5e60
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-11-13 12:41:07 -0500

    [index] add /about and /blog

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.send('Hello, SEI world');
+});
+
+
+app.get('/about', function(req, res) {
+    res.send('This is the about page.')
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Welcomne to my blog');
 })
 
 app.listen(8000);
